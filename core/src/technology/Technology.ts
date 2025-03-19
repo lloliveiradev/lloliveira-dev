1a0d07d893e79f6809c1c7b5e6d5188d720eca7c
Leonardo Oliveira <leonardo@apoiotech.com.br>
2025-03-18 21:30:08 -0300

    feat(core): clean model

--- core/src/technology/Technology.ts
+++ core/src/technology/Technology.ts
@@ -5,12 +5,3 @@ export default interface Technology {
     image: string
     highlight: boolean
 }
-
-const t: Partial<Technology> = {
-    name: 'JavaScript',
-    description: 'JavaScript is a programming language that conforms to the ECMAScript specification. JavaScript is high-level, often just-in-time compiled, and multi-paradigm. It has curly-bracket syntax, dynamic typing, prototype-based object-orientation, and first-class functions.',
-    image: 'https://upload.wikimedia.org/wikipedia/commons/6/6a/JavaScript-logo.png',
-    highlight: true
-}
-
-console.log(t);
