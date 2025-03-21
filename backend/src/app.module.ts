d93fdedbd11ae0d13b33580c52bf44e634d755d5
Leonardo Oliveira <leonardo@apoiotech.com.br>
2025-03-21 16:34:52 -0300

    feat(backend): deploy configs

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,11 +1,21 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { DbModule } from './db/db.module';
-import { TechnologyModule } from './technology/technology.module';
-import { ProjectModule } from './project/project.module';
+import { Module } from "@nestjs/common"
+import { AppController } from "./app.controller"
+import { DbModule } from "./db/db.module"
+import { TechnologyModule } from "./technology/technology.module"
+import { ProjectModule } from "./project/project.module"
+import { ServeStaticModule } from "@nestjs/serve-static"
+import { join } from "path"
 
 @Module({
-  imports: [DbModule, TechnologyModule, ProjectModule],
+  imports: [
+    DbModule,
+    TechnologyModule,
+    ProjectModule,
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, "../frontend", "dist"),
+      exclude: ["/api*"],
+    }),
+  ],
   controllers: [AppController],
 })
 export class AppModule { }
